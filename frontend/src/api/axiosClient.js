aca3142707119a94160cdabd32a744f0dcdfa448
Ariel Arcentales <amarcentales@puce.edu.ec>
2026-07-23 14:55:22 -0500

    Arreglo de los caracteres no renocidos

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+let envUrl = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+
+const cleanUrl = envUrl.replace(/['"[\]]/g, "").trim();
+
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  baseURL: cleanUrl,
 });
+
 axiosClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
